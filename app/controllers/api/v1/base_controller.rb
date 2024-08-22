974e6d0be59b0c7dcdd778fd18d035578ecf846d
“seyhadot” <seyhadot@gmail.com>
2024-08-22 09:51:55 +0700

    setup user logout

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,6 +1,2 @@
 class Api::V1::BaseController < ApplicationController
-  include ActionController::MimeResponds
-  include ActionController::Cookies
-  before_action :authenticate_user!
-  respond_to :json
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -18,25 +18,22 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   def respond_to_on_destroy
-    if request.headers['Authorization'].present?
+    jwt_token = request.headers['Authorization']&.split(' ')&.last
+    if jwt_token
       begin
-        jwt_payload = JWT.decode(request.headers['Authorization'].split(' ').last, ENV['DEVISE_JWT_SECRET_KEY']).first
-        current_user = User.find_by(id: jwt_payload['sub'])
-      rescue JWT::DecodeError, ActiveRecord::RecordNotFound
-        current_user = nil
+        jwt_payload = JWT.decode(jwt_token, ENV['DEVISE_JWT_SECRET_KEY'], true, { algorithm: 'HS256' }).first
+        current_user = User.find(jwt_payload['user_id'])
+        current_user.update(jti: SecureRandom.uuid)
+        render json: { status: 200, message: 'Logged out successfully.' }, status: :ok
+      rescue JWT::DecodeError => e
+        render json: { status: 401, message: "Invalid token: #{e.message}" }, status: :unauthorized
+      rescue ActiveRecord::RecordNotFound
+        render json: { status: 404, message: "User not found" }, status: :not_found
+      rescue StandardError => e
+        render json: { status: 500, message: "An error occurred: #{e.message}" }, status: :internal_server_error
       end
-    end
-
-    if current_user
-      render json: {
-        status: 200,
-        message: 'Logged out successfully.'
-      }, status: :ok
     else
-      render json: {
-        status: 401,
-        message: "Couldn't find an active session."
-      }, status: :unauthorized
+      render json: { status: 401, message: "No token provided." }, status: :unauthorized
     end
   end
 
